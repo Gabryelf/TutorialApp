0036faf1e7df4dfed37777530ede803a8d58346d
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-08-24 15:55:45 +0300

    add_proj_ado_connect
    start proj

---
+++ AdoNet/AppConnect/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AppConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Program");
+            Console.ReadKey();
+
+            // Строка подключения к SQL Server
+            string connectionString = "Server=localhost;Database=TraneeDB;Integrated Security=True;";
+
+            try
+            {
+                using ()
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка подключения: {ex.Message}");
+            }
+            finally 
+            {
+                Console.WriteLine("Закрываем соединение!");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+
